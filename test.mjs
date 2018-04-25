8618e49ed9bb4b831a7ee4e90ab68a22462db49f
Ben Pardo <benpardo@bens-mbp-4.lan>
2018-04-25 01:13:47 -0600

    Tests Spanish greeting.

--- test.mjs
+++ test.mjs
@@ -3,4 +3,5 @@ import {makeGreeting} from "./index";
 
 assert.equal(makeGreeting(), "Hello world!");
 assert.equal(makeGreeting("Ben"), "Hello Ben!");
+assert.equal(makeGreeting(), "\u00a1Hola mundo!");
 console.log("Success");
