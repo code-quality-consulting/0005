01d84cf9e15c3869bdf0e1f7a022874b68e261a2
Ben Pardo <benpardo@Bens-MacBook-Pro-4.local>
2018-04-25 20:39:50 -0600

    Tests partial application for Spanish.

--- test.mjs
+++ test.mjs
@@ -6,7 +6,9 @@ import {makeGreeting} from "./index";
 import {makeGreeter} from "./index";
 
 const makeEnglishGreeting = makeGreeter("English");
+const makeSpanishGreeting = makeGreeter("Spanish");
+
 assert.equal(makeEnglishGreeting(), "Hello world!");
 assert.equal(makeEnglishGreeting("Ben"), "Hello Ben!");
-assert.equal(makeGreeting("", "Spanish"), "\u00a1Hola mundo!");
+assert.equal(makeSpanishGreeting(), "\u00a1Hola mundo!");
 console.log("Success");
