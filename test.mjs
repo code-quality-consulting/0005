838ce593b9cdd2a12c8070959e83aaf24b5b702c
Ben Pardo <visaudi@gmail.com>
2018-04-26 15:54:17 -0600

    Tests Russian greeting.

--- test.mjs
+++ test.mjs
@@ -6,8 +6,10 @@ import {makeGreeter} from "./index";
 
 const makeEnglishGreeting = makeGreeter("English");
 const makeSpanishGreeting = makeGreeter("Spanish");
+const makeRussianGreeting = makeGreeter("Russian");
 
 assert.equal(makeEnglishGreeting(), "Hello world!");
 assert.equal(makeEnglishGreeting("Ben"), "Hello Ben!");
 assert.equal(makeSpanishGreeting(), "\u00a1Hola mundo!");
+assert.equal(makeRussianGreeting(), "Привет мир!");
 console.log("Success");
